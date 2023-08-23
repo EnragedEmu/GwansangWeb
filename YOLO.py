e992b9d7869dccf495655f6a48b1b83cb7e2cc47
youuor7r <125183418+youuor7r@users.noreply.github.com>
2023-08-23 20:13:00 +0900

    Update YOLO.py

--- YOLO.py
+++ YOLO.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 import yaml
 
+#YOLO 실행 함수
 def YOLO_predict(path):
+    #모델은 best.pt,실행하는 동작은 data.yaml이 판단한다. img는 jpg파일로(png도 받을 수 있게 수정 요망) 
     model_path = f'{path}/best.pt'
     image_path = f'{path}/img.jpg'
     yaml_path = f'{path}/data.yaml'
@@ -15,5 +17,5 @@ def YOLO_predict(path):
 
     return result
 
-path='D:/pycharm project/my project/faceMBTI/practice'
+path='실제 path 넣으시오'
 YOLO_predict(path)
