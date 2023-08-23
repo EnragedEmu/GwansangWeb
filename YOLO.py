0a163424a14b9186c2446c78a31c97832f2d9375
youuor7r <125183418+youuor7r@users.noreply.github.com>
2023-08-23 20:56:06 +0900

    Update YOLO.py

--- YOLO.py
+++ YOLO.py
@@ -2,10 +2,10 @@ from ultralytics import YOLO
 import yaml
 
 #YOLO 실행 함수
-def YOLO_predict(path):
+def YOLO_predict(path,img):
+    
     #모델은 best.pt,실행하는 동작은 data.yaml이 판단한다. img는 jpg파일로(png도 받을 수 있게 수정 요망) 
     model_path = f'{path}/best.pt'
-    image_path = f'{path}/img.jpg'
     yaml_path = f'{path}/data.yaml'
 
     model = YOLO(model_path)
@@ -13,9 +13,16 @@ def YOLO_predict(path):
     with open(yaml_path, 'r') as f:
         face_yaml = yaml.safe_load(f)
 
-    result = model.predict(source=image_path, save=True,save_crop=True)
+    #img를 모델에 돌린다.
+    result = model.predict(source=img)
 
-    return result
+    #result에서 boxes의 x,y 좌표들을 구해본다.
+    for r in result:
+        coordinate = r.boxes.xyxy
 
-path='실제 path 넣으시오'
-YOLO_predict(path)
+    #좌표값 받아오기
+    x1, y1, x2, y2 = map(int, coordinate[0])
+
+    cropped_image = img[y1:y2, x1:x2]
+
+    return cropped_image
